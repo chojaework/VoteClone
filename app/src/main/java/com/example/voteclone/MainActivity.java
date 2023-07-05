1e975f015e540239392db0106ec0261bc9c2237c
hyunsoo <top010301@kaist.ac.kr>
2023-07-05 14:31:00 +0900

    vote commit

--- app/src/main/java/com/example/voteclone/MainActivity.java
+++ app/src/main/java/com/example/voteclone/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.voteclone;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,7 +19,9 @@ public class MainActivity extends AppCompatActivity {
     int vCount[] = new int[3];
     ImageView[] imgV = new ImageView[3];
     int[] imgID = {R.id.img1, R.id.img2, R.id.img3};
-    String Name[] = {"재원","현수","정원"};
+    String Candidate[] = {"재원","현수","정원"};
+    String Voter[] = {};
+    private EditText et_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         final int[] VotePeople = {0};
         TextView num = findViewById(R.id.textView2);
 
+        et_name = findViewById(R.id.et_name);
         btn = (Button)findViewById(R.id.btn_endvote);
 
         for(int i = 0; i < imgID.length; i++) {
@@ -36,10 +42,34 @@ public class MainActivity extends AppCompatActivity {
             imgV[index].setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    VotePeople[0]++;
-                    vCount[index]++;
-                    Toast.makeText(getApplicationContext(), Name[index] + "에게 투표하셨습니다.",Toast.LENGTH_SHORT).show();
-                    num.setText( VotePeople[0] + "명");
+                    String name = et_name.getText().toString().trim();
+                    if (name.isEmpty()) {
+                        Toast.makeText(getApplicationContext(), "이름을 입력하세요", Toast.LENGTH_SHORT).show();
+                    } else {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setMessage("투표하시겠습니까?")
+                                .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        String[] updatedVoter = new String[Voter.length + 1];
+                                        System.arraycopy(Voter, 0, updatedVoter, 0, Voter.length);
+                                        updatedVoter[Voter.length] = name;
+                                        Voter = updatedVoter;
+
+                                        et_name.setText("");
+                                        VotePeople[0]++;
+                                        vCount[index]++;
+                                        Toast.makeText(getApplicationContext(), Candidate[index] + "에게 투표하셨습니다.", Toast.LENGTH_SHORT).show();
+                                        num.setText(VotePeople[0] + "명");
+                                    }
+                                })
+                                .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        // 아무 변화 없음
+                                    }
+                                });
+                        AlertDialog dialog = builder.create();
+                        dialog.show();
+                    }
                 }
             });
         }
@@ -47,9 +77,11 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public  void onClick(View view) {
+
                 Intent intent = new Intent(getApplicationContext(), ResultVote.class);
                 intent.putExtra("vCount",vCount);
-                intent.putExtra("Name",Name);
+                intent.putExtra("Candidate", Candidate);
+                intent.putExtra("Voter", Voter);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/voteclone/ResultVote.java
+++ app/src/main/java/com/example/voteclone/ResultVote.java
@@ -1,14 +1,28 @@
 package com.example.voteclone;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+
+import java.io.IOException;
 
 public class ResultVote extends Activity {
     @Override
@@ -19,11 +33,16 @@ public class ResultVote extends Activity {
 
         ImageView resImg = (ImageView) findViewById(R.id.resImg);
         TextView resText = (TextView) findViewById(R.id.tv1);
-        int top = 0;
+        final ListView lv = (ListView) findViewById(R.id.lv_voter);
 
+        int top = 0;
         Intent intent = getIntent();
         int[] rVote = intent.getIntArrayExtra("vCount");
-        String[] rName = intent.getStringArrayExtra("Name");
+        String[] rCandidate = intent.getStringArrayExtra("Candidate");
+        String[] rVoter = intent.getStringArrayExtra("Voter");
+
+        ArrayAdapter<String> lAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, rVoter);
+        lv.setAdapter(lAdapter);
 
         int rImg[] = {R.drawable.pic1, R.drawable.pic2, R.drawable.pic3};
 
@@ -33,7 +52,7 @@ public class ResultVote extends Activity {
 
         rVote.toString();
         resImg.setImageResource(rImg[top]);
-        resText.setText(rName[top]+"/"+rVote[top]+"표");
+        resText.setText(rCandidate[top]+"/"+rVote[top]+"표");
 
         Button btn = findViewById(R.id.rbtn);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -43,4 +62,5 @@ public class ResultVote extends Activity {
             }
         });
     }
+
 }
